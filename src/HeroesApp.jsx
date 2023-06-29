bee01ad7d919f4476ed5741c5160ac6c50648a89
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-29 03:45:28 -0600

    Refactor

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,13 +1,5 @@
-import { AuthProvider } from './auth';
-import { AppRouter } from './router/AppRouter';
-
+import { AppRouter } from "./router/AppRouter";
 
 export const HeroesApp = () => {
-  return (
-    <AuthProvider>
-        
-        <AppRouter />
-        
-    </AuthProvider>
-  )
-}
+  return <AppRouter />;
+};
