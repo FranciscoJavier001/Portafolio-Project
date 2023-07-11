26d25b2cf4d8c8ac44237804524d4a90f4e4d9fa
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-11 16:46:55 -0600

    Annotations

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,13 +1,15 @@
-import { HeroList } from '../components';
-
+import { HeroList } from "../components"; //* Importo la lista de Heroes */
 
 export const DcPage = () => {
+  //* Exporto esta Pagina */
   return (
+    //* Muestro en el DOM */
     <>
-      <h1>DC Comics</h1>
-      <hr />
-
-      <HeroList publisher='DC Comics' />
+      <h1 className="text-center mt-1">DC Comics</h1>
+      {/* Un Titulo con una clase */}
+      <hr /> {/* Una linea */}
+      <HeroList publisher="DC Comics" />
+      {/* Utilizamos la logica de este componente, donde la editorial sea */}
     </>
-  )
-}
+  );
+};
