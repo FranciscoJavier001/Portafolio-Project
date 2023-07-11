219ed88cf1eabb69755ce4d61ebb78eb79fffdfb
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-11 14:21:13 -0600

    Annotations

--- src/heroes/helpers/getHeroesByPublisher.js
+++ src/heroes/helpers/getHeroesByPublisher.js
@@ -1,10 +1,13 @@
-import { heroes } from "../data/heroes"; //* Mando llamar la data de los heroes */
+import { heroes } from "../data/heroes"; //* Data de Heroes */
 
 export const getHeroesByPublisher = (publisher) => {
-  const validPublishers = ["DC Comics", "Marvel Comics"];
+  //* Exporto esta variable, que recibe el publisher */
+  const validPublishers = ["DC Comics", "Marvel Comics"]; //* Nueva variable, es un arreglo con 2 elementos */
   if (!validPublishers.includes(publisher)) {
-    throw new Error(`${publisher} is not a valid publisher`);
+    //* Si es diferente la editorial valida que viene incluida en la editorial */
+    throw new Error(`${publisher} No es una Editorial Valida`); //* Muestra este Error */
   }
 
   return heroes.filter((heroe) => heroe.publisher === publisher);
+  //* Si es valido retorna la data de heroes pero filtra el arreglo en una nueva variable que sea la editorial y debe ser igual a la editorial */
 };
