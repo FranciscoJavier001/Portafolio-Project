1e2c670dc364204b40d04ff19b2971688ecb760f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-11 14:40:07 -0600

    Annotations

--- src/heroes/helpers/getHeroById.js
+++ src/heroes/helpers/getHeroById.js
@@ -1,7 +1,7 @@
-import { heroes } from '../data/heroes';
+import { heroes } from "../data/heroes"; //* Importo la data de los Heroes */
 
-
-export const getHeroById = ( id ) => {
-
-    return heroes.find( hero => hero.id === id );
-}
+export const getHeroById = (id) => {
+  //* Exporto esta funcion de busqueda, que recibe el id */
+  return heroes.find((hero) => hero.id === id);
+  //* Retorno un arreglo, que encuentra una variable y me regresa el id, si es igual al id */
+};
