dc9b1ab7a709a7d18cebbfc581c1177cb36f9755
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-11 14:07:52 -0600

    Annotations

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,15 +1,24 @@
-import { useMemo } from "react";
-import { HeroCard } from "./";
+import { useMemo } from "react"; //* Es un hook de React */
+import { HeroCard } from "./"; //* Importo la pagina */
 import { getHeroesByPublisher } from "../helpers";
 
 export const HeroList = ({ publisher }) => {
+  //* Exporto esta constante con una variable a recibir */
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  //* Nueva variable, utilize el hook useMemo, su dependencia es publisher y debe estar atenta a los cambios en el publisher */
 
   return (
-    <div className="row rows-cols-1 row-cols-md-3 g-3">
-      {heroes.map((hero) => (
-        <HeroCard key={hero.id} {...hero} />
-      ))}
+    //* Lo que retornamos */
+    <div className="row rows-cols-1 row-cols-md-3 g-2">
+      {/* Como mostramos las tarjetas */}
+      {heroes.map(
+        //* Mapeamos la constante heroes */
+        (
+          hero //* Nueva variable que sera el nuevo arreglo */
+        ) => (
+          <HeroCard key={hero.id} {...hero} /> //* Renderizamos HeroCard, la llave es el id y mostramos todo el arreglo */
+        )
+      )}
     </div>
   );
 };
