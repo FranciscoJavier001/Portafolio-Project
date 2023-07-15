539aa21c16149c666a527b3ef6b98f409f2dda09
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-15 07:46:16 -0600

    Annotations

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,13 +1,15 @@
-import { HeroList } from '../components';
+import { HeroList } from "../components"; //* Importo la logica de este componente */
 
 export const MarvelPage = () => {
+  //* Exporto esta funcion que no recibe nada */
   return (
+    //* Lo que renderizo */
     <>
-      <h1>Marvel Comics</h1>
-      <hr />
-
-      <HeroList publisher='Marvel Comics' />
-
+      <h1 className="text-center">Marvel Comics</h1>
+      {/* Titulo del componente */}
+      <hr /> {/* Una linea */}
+      <HeroList publisher="Marvel Comics" />
+      {/* La funcion que importe, va a recibir esta variable */}
     </>
-  )
-}
+  );
+};
