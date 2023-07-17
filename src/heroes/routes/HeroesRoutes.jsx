3272787fb03208a373b9fe6a3ba9cfae03b4dc2a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-16 21:44:57 -0600

    Annotations

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,27 +1,23 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Navbar } from '../../ui';
-import { DcPage, HeroPage, MarvelPage, SearchPage } from '../pages';
+import { Navigate, Route, Routes } from "react-router-dom"; //* De RRD */
+import { Navbar } from "../../ui"; //* Logica del Navbar */
+import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages";
 
 export const HeroesRoutes = () => {
   return (
     <>
-        <Navbar />
+      <Navbar />
 
-        <div className="container">
-            <Routes>
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
-                
-                <Route path="search" element={<SearchPage />} />
-                <Route path="hero/:id" element={<HeroPage />} />
-                                
+      <div className="container">
+        <Routes>
+          <Route path="marvel" element={<MarvelPage />} />
+          <Route path="dc" element={<DcPage />} />
 
-                <Route path="/" element={<Navigate to="/marvel" />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="hero/:id" element={<HeroPage />} />
 
-            </Routes>
-        </div>
-
-
+          <Route path="/" element={<Navigate to="/marvel" />} />
+        </Routes>
+      </div>
     </>
-  )
-}
+  );
+};
