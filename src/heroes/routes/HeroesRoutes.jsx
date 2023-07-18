bf7adbef49e9a4b909b8eaf46efede14fa3f543c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-17 22:46:19 -0600

    Annotations in Routes

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -1,21 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom"; //* De RRD */
 import { Navbar } from "../../ui"; //* Logica del Navbar */
-import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages";
+import { DcPage, HeroPage, MarvelPage, SearchPage } from "../pages"; //* Logica de los componentes funcionales */
 
 export const HeroesRoutes = () => {
+  //* Exporto esta funcion */
   return (
+    //* Lo que renderizo en el DOM */
     <>
-      <Navbar />
-
+      <Navbar /> {/* Renderizo este Componente Funcional */}
       <div className="container">
+        {/* Div con la clase Container */}
         <Routes>
+          {/* Rutas */}
           <Route path="marvel" element={<MarvelPage />} />
+          {/* Cuando sea esta ruta en el URL renderiza este Componente Funcional */}
           <Route path="dc" element={<DcPage />} />
-
+          {/* Cuando sea esta ruta en el URL renderiza este Componente Funcional */}
           <Route path="search" element={<SearchPage />} />
+          {/* Cuando sea esta ruta en el URL renderiza este Componente Funcional */}
           <Route path="hero/:id" element={<HeroPage />} />
-
+          {/* Cuando sea esta ruta en el URL renderiza este Componente Funcional */}
           <Route path="/" element={<Navigate to="/marvel" />} />
+          {/* Cuando no exista la URL renderiza este Componente Funcional */}
         </Routes>
       </div>
     </>
