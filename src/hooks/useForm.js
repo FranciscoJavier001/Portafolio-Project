56ba097b402c1208d710462b7f627afd44b99887
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-19 15:20:47 -0600

    Annotations

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,25 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react"; //* Hook de React */
 
-export const useForm = ( initialForm = {} ) => {
-  
-    const [ formState, setFormState ] = useState( initialForm );
+export const useForm = (initialForm = {}) => {
+  //* Exportamos esta funcion, que recibe un parametro vacio */
+  const [formState, setFormState] = useState(initialForm); //* Hook useState que recibe 2 parametros y su parametro inicial vacio */
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [ name ]: value
-        });
-    }
+  const onInputChange = ({ target }) => {
+    //* Definimos una variable que recibe un parametro */
+    const { name, value } = target; //* Variables del parametro tarjet */
+    setFormState({
+      //* Cuando cambia este parametro */
+      ...formState, //* Regreso este arreglo con todos los resultados */
+      [name]: value, //* Nueva propiedad del objeto name con el valor de la propiedad value */
+    });
+  };
 
-    const onResetForm = () => {
-        setFormState( initialForm );
-    }
+  const onResetForm = () => {
+    //* Nueva variable que no recibe nada */
+    setFormState(initialForm); //* Variable del hook useState que pone el formulario en su forma inicial que es vacia */
+  };
 
-    return {
-        ...formState,
-        formState,
-        onInputChange,
-        onResetForm,
-    }
-}
+  return {
+    //* Lo que voy a regresar */
+    ...formState, //* Regreso el arreglo pero con lo que tiene dentro que es su estado anterior */
+    formState, //* Nuevo estado del formulario */
+    onInputChange, //* Funcion */
+    onResetForm, //* Funcion */
+  };
+};
