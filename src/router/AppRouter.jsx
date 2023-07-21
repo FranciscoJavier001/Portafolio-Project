160af031da8b55672177c03ed87f9166ef7ea511
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-20 19:09:10 -0600

    Annotations

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-
-import { HeroesRoutes } from "../heroes";
+import { Route, Routes } from "react-router-dom"; //* Importamos para hacer uso de las rutas */
+import { HeroesRoutes } from "../heroes"; //* Rutas en el Navbar */
 
 export const AppRouter = () => {
+  //* Exportamos esta variable, que sera puesto en el Main y es lo que vamos a renderizar */
   return (
+    //* Lo que voy a renderizar */
     <>
       <Routes>
+        {/* Invocamos a las turas */}
         <Route path="/*" element={<HeroesRoutes />} />
+        {/* Lo que sea la ruta que renderize el Elemento HeroesRoutes */}
       </Routes>
     </>
   );
