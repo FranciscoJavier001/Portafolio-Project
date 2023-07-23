ca978c93c2e6f9c74682192bae10a763b5f4de88
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-22 22:22:00 -0600

    Annotations

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,36 +1,42 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom"; //* Poder navegar entre URL's */
 
 export const Navbar = () => {
+  //* Variable a Exportar */
   return (
+    //* Lo que voy a renderizar */
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+      {/* Esta es la barra de navegacion con clases de Bootstrap */}
       <Link className="navbar-brand" to="/">
-        Asociaciones
+        {/* Es el Brand, osea el primer enlace */}
+        HeroesApp {/* Le cambie el nombre */}
       </Link>
-
-      <div className="navbar-collapse">
+      <div>
+        {/* Simplemente un div */}
         <div className="navbar-nav">
+          {/* Clase, para que esten todos alineados horizontalmente */}
           <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link  ${isActive ? "active" : ""}`
-            }
+            className={(
+              { isActive } //* Clase que recibe un parametro */
+            ) => `nav-item nav-link  ${isActive ? "active" : ""}`} //* Donde este seleccionado, ponlo en color blanco y dejalo gris */
             to="/marvel">
-            Marvel
+            {/* Ruta a donde te voy a redirigir */}
+            Marvel {/* Lo que dice el espacio en el navbar */}
           </NavLink>
-
           <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link  ${isActive ? "active" : ""}`
-            }
+            className={(
+              { isActive } //* Clase que recibe un parametro */
+            ) => `nav-item nav-link  ${isActive ? "active" : ""}`} //* Donde este seleccionado, ponlo en color blanco y dejalo gris */
             to="/dc">
-            DC
+            {/* Ruta a donde te voy a redirigir */}
+            DC {/* Lo que dice el espacio en el navbar */}
           </NavLink>
-
           <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link  ${isActive ? "active" : ""}`
-            }
+            className={(
+              { isActive } //* Clase que recibe un parametro */
+            ) => `nav-item nav-link  ${isActive ? "active" : ""}`} //* Donde este seleccionado, ponlo en color blanco y dejalo gris */
             to="/search">
-            Search
+            {/* Ruta a donde te voy a redirigir */}
+            Search {/* Lo que dice el espacio en el navbar */}
           </NavLink>
         </div>
       </div>
