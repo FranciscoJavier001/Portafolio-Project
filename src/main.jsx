8b6847451998297f7e78e8e340253ae2d80f23bc
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-24 17:07:10 -0600

    Annotations

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react"; //* Importacion para usar React */
+import ReactDOM from "react-dom/client"; //* Importacion de lo que se renderiza en el VirtualDom */
+import { BrowserRouter } from "react-router-dom"; //* Importacion para navegar entre rutas */
 
-import { HeroesApp } from './HeroesApp';
-import './styles.css';
+import { HeroesApp } from "./HeroesApp"; //* Logica de este Componente Funcional */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //* Como voy a renderzar en el VirtualDom */
   <React.StrictMode>
+    {/* Aplicamos el modo estricto */}
     <BrowserRouter>
-      <HeroesApp />
+      {/* Hacia que ruta vamos a navegar */}
+      <HeroesApp /> {/* La App qie voy a renderizar */}
     </BrowserRouter>
   </React.StrictMode>
 );
