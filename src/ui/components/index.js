eceab8ac8a4d4225a888b8114bd2efbffecfee6e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-25 18:31:43 -0600

    Imports

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,1 +1,3 @@
 export * from "./Navbar"; //* Exporto este Componente Funcional */
+export * from "./Footer"; //* Exporto este Componente Funcional */
+export * from "./FooterPosition"; //* Exporto este Componente Funcional */
