06c352450047d0630462eed77f0a9e3053e0b6f4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-25 18:30:17 -0600

    Add Container and Margins

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -4,6 +4,7 @@ import queryString from "query-string";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
 import { getHeroesByName } from "../helpers";
+import { FooterPosition } from "../../ui";
 
 export const SearchPage = () => {
   //* Funcion de Flecha que no recibe Nada */
@@ -31,63 +32,69 @@ export const SearchPage = () => {
   return (
     //* Retorno */
     <>
-      {/* Para meterle formato al DOM */}
-      <h1 className="text-center">PaginaBusqueda</h1>
-      {/* Titulo en la pagina, lo coloco al centro */}
-      <hr /> {/* Una raya */}
-      <div className="row">
-        {/* Nueva fila */}
-        <div className="col-5">
-          {/* Espacio de lo que va a medir todo lo que esta dentro de este div */}
-          <h4 className="text-center">Busqueda</h4>
-          {/* Es el subtitulo */}
-          <hr /> {/* Otra raya */}
-          <form onSubmit={onSearchSubmit} aria-label="form">
-            {/* Formulario, que recibe un evento y le damos estilos al formulario definidos */}
-            <input //* Ahora al campo de busqueda */
-              type="text" //* Lo que recibe es texto */
-              placeholder="Busca un Heroe" //* Lo que dice escrito como de fondo */
-              className="form-control" //* Va a tomar esto que al parecer ya esta definido, lo utilizo para usar todo el ancho del espacio */
-              name="searchText" //* Asi se va a llamar este campo */
-              autoComplete="off" //* No vas a autocompletar */
-              value={searchText} //* Este es el nombre del campo, y tambien este es su valor, asi se lo asigno */
-              onChange={onInputChange} //* Si preciono el Enter, automaticamente inicia la busqueda */
-            />
-            <button className="btn btn-outline-primary mt-1 form-control">
-              {/* Boton que ocupa todo el espacio */}
-              Buscar {/* Texto que dice el boton */}
-            </button>
-          </form>
-        </div>
-        <div className="col-7">
-          {/* Quedaban 7 espacios, los asigno aqui */}
-          <h4 className="text-center">Resultados</h4>
-          {/* Es un titulo en el espacio resultante con texto */}
-          <hr /> {/* Simplemente es una linea horizontal */}
-          <div
-            className="alert alert-primary animate__animated animate__fadeIn text-center" //* Division Azul del cuadro de busqueda */
-            style={{ display: showSearch ? "" : "none" }}>
-            {/* Campo de busqueda vacio, muestro recuadro azul, o si hay busqueda lo desaparezco */}
-            Busca un Heroe {/* Es un texto */}
+      <div className="container">
+        {/* Para meterle formato al DOM */}
+        <h1 className="text-center">PaginaBusqueda</h1>
+        {/* Titulo en la pagina, lo coloco al centro */}
+        <hr /> {/* Una raya */}
+        <div className="row">
+          {/* Nueva fila */}
+          <div className="col-5">
+            {/* Espacio de lo que va a medir todo lo que esta dentro de este div */}
+            <h4 className="text-center">Busqueda</h4>
+            {/* Es el subtitulo */}
+            <hr /> {/* Otra raya */}
+            <form onSubmit={onSearchSubmit} aria-label="form">
+              {/* Formulario, que recibe un evento y le damos estilos al formulario definidos */}
+              <input //* Ahora al campo de busqueda */
+                type="text" //* Lo que recibe es texto */
+                placeholder="Busca un Heroe" //* Lo que dice escrito como de fondo */
+                className="form-control" //* Va a tomar esto que al parecer ya esta definido, lo utilizo para usar todo el ancho del espacio */
+                name="searchText" //* Asi se va a llamar este campo */
+                autoComplete="off" //* No vas a autocompletar */
+                value={searchText} //* Este es el nombre del campo, y tambien este es su valor, asi se lo asigno */
+                onChange={onInputChange} //* Si preciono el Enter, automaticamente inicia la busqueda */
+              />
+              <button className="btn btn-outline-primary mt-1 form-control">
+                {/* Boton que ocupa todo el espacio */}
+                Buscar {/* Texto que dice el boton */}
+              </button>
+            </form>
           </div>
-          <div
-            aria-label="alert-danger" //* Son estilos definidos para este campo */
-            className="alert alert-danger animate__animated animate__fadeIn text-center" //* Division del cuadro rojo con estilos definidos */
-            style={{ display: showError ? "" : "none" }}>
-            {/* Si no encuentra nada que se muestre, pero si encuentra algo que desaparezca */}
-            No Encontre Ningun Heroe con la letra <b>{`"${q}"`}</b>
-            {/* Esto es un texto */}
+          <div className="col-7">
+            {/* Quedaban 7 espacios, los asigno aqui */}
+            <h4 className="text-center">Resultados</h4>
+            {/* Es un titulo en el espacio resultante con texto */}
+            <hr /> {/* Simplemente es una linea horizontal */}
+            <div
+              className="alert alert-primary animate__animated animate__fadeIn text-center" //* Division Azul del cuadro de busqueda */
+              style={{ display: showSearch ? "" : "none" }}>
+              {/* Campo de busqueda vacio, muestro recuadro azul, o si hay busqueda lo desaparezco */}
+              Busca un Heroe {/* Es un texto */}
+            </div>
+            <div
+              aria-label="alert-danger" //* Son estilos definidos para este campo */
+              className="alert alert-danger animate__animated animate__fadeIn text-center" //* Division del cuadro rojo con estilos definidos */
+              style={{ display: showError ? "" : "none" }}>
+              {/* Si no encuentra nada que se muestre, pero si encuentra algo que desaparezca */}
+              No Encontre Ningun Heroe con la letra <b>{`"${q}"`}</b>
+              {/* Esto es un texto */}
+            </div>
+            {heroes.map(
+              //* Voy a mapear el arreglo de heroes */
+              (
+                hero //* Recibo la variable hero */
+              ) => (
+                <HeroCard key={hero.id} {...hero} /> //* Renderizo este componente, la llave va a ser el "id" y muestro todos los resultados */
+              )
+            )}
           </div>
-          {heroes.map(
-            //* Voy a mapear el arreglo de heroes */
-            (
-              hero //* Recibo la variable hero */
-            ) => (
-              <HeroCard key={hero.id} {...hero} /> //* Renderizo este componente, la llave va a ser el "id" y muestro todos los resultados */
-            )
-          )}
         </div>
       </div>
+      <div className="m-5">
+        <br />
+      </div>
+      <FooterPosition />
     </>
   );
 };
