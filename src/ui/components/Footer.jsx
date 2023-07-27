bd1a823723540c4734009affc7d83a8ab9ae0bcc
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-26 18:59:17 -0600

    Icons

--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -1,14 +1,20 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { BiCodeAlt } from "react-icons/bi";
+
 export const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center pt-1 pb-1">
       <p>
         &copy; Francisco Javier Martinez Duran
         <br />
-        Fecha de Creacion: <span id="date"></span>
+        Fecha de Creacion: <span>Fecha</span>
         <br />
-        Fecha Actual: <span id="date1"></span>
+        Fecha Actual: <span>Fecha</span>
+        <br />
+        <BsLinkedin />
+        <BiCodeAlt />
+        <BsGithub />
       </p>
-      <a href="/about">Ir a la página Acerca de</a>
     </footer>
   );
 };
