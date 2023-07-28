6c0f2dc5d5413140f3d5d0a37efa2f15457e5fa8
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-27 18:20:26 -0600

    Updated Design

--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -4,17 +4,15 @@ import { BiCodeAlt } from "react-icons/bi";
 export const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center pt-1 pb-1">
-      <p>
-        &copy; Francisco Javier Martinez Duran
-        <br />
-        Fecha de Creacion: <span>Fecha</span>
-        <br />
-        Fecha Actual: <span>Fecha</span>
-        <br />
-        <BsLinkedin />
-        <BiCodeAlt />
-        <BsGithub />
-      </p>
+      &copy; Francisco Javier Martinez Duran
+      <br />
+      Fecha de Creacion: <span></span>
+      <br />
+      Fecha Actual: <span></span>
+      <br />
+      <BsLinkedin />
+      <BiCodeAlt />
+      <BsGithub />
     </footer>
   );
 };
--- src/ui/components/FooterPosition.jsx
+++ src/ui/components/FooterPosition.jsx
@@ -1,21 +1,18 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { BiCodeAlt } from "react-icons/bi";
+
 export const FooterPosition = () => {
   return (
     <footer className="bg-dark text-white text-center pt-1 pb-1 fixed-bottom">
-      <p>
-        &copy; Francisco Javier Martinez Duran
-        <br />
-        Fecha de Creacion: <span id="date"></span>
-        <br />
-        Fecha Actual: <span id="date1"></span>
-      </p>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/francisco-javier-martinez-duran-92055b275/"
-          target="_blank"
-          rel="noopener noreferrer">
-          {/* <i class="fa fa-linkedin"></i> */}
-        </a>
-      </div>
+      &copy; Francisco Javier Martinez Duran
+      <br />
+      Fecha de Creacion: <span></span>
+      <br />
+      Fecha Actual: <span></span>
+      <br />
+      <BsLinkedin />
+      <BiCodeAlt />
+      <BsGithub />
     </footer>
   );
 };
