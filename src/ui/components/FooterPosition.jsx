a31538e64fdc5bd0faaa6c9c6a18a4e7a09065d1
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-07-30 22:35:11 -0600

    Styles

--- src/ui/components/FooterPosition.jsx
+++ src/ui/components/FooterPosition.jsx
@@ -1,35 +1,45 @@
-import { BsLinkedin, BsGithub } from "react-icons/bs";
-import { BiCodeAlt } from "react-icons/bi";
+import { BsGithub } from "react-icons/bs";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { PiCodeBold } from "react-icons/pi";
+
+import styled from "styled-components";
+
+const ATagLink = styled.a`
+  color: white; /* La tipografia de las letras es blanca */
+  margin: 1rem; /* Separacion de los iconos sociables */
+  font-size: 1.9rem; /* Tamaño de cada elemento */
+  line-height: 3vh; /* Espacio de cada linea */
+`;
 
 export const FooterPosition = () => {
   return (
-    <footer className="bg-dark text-white text-center pt-1 pb-1 fixed-bottom">
+    <footer className="bg-dark text-white text-center pt-1 pb-2 fixed-bottom">
       &copy; Francisco Javier Martinez Duran
       <br />
       Fecha de Creacion: <span></span>
       <br />
       Fecha Actual: <span></span>
       <br />
-      <a
+      <ATagLink
         href="https://www.linkedin.com/in/francisco-javier-martinez-duran-92055b275"
         target="_blank"
         rel="noopener noreferrer">
-        <BsLinkedin />
-      </a>
-      <a
+        <BiLogoLinkedin />
+      </ATagLink>
+      <ATagLink
         href="https://github.com/FranciscoJavier001/Portafolio-Project"
         target="_blank"
         rel="noopener noreferrer">
-        <BiCodeAlt />
-      </a>
-      <a
+        <PiCodeBold />
+      </ATagLink>
+      <ATagLink
         href="https://github.com/FranciscoJavier001"
         target="_blank"
         rel="noopener noreferrer">
         <BsGithub />
-      </a>
+      </ATagLink>
     </footer>
   );
 };
 
-//* ESTILOS PARA ESTOS ELEMENTO, UN LINK, UN HOVER */
+//* Hay que instalar Stuled-components */
