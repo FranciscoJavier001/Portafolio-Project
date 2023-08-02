3bb2bc1ef1dc6ef34875e3bbeee3f8bf88f6f94c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-08-01 19:26:17 -0600

    Export Functional Components

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,3 +1,5 @@
 export * from "./Navbar"; //* Exporto este Componente Funcional */
 export * from "./Footer"; //* Exporto este Componente Funcional */
 export * from "./FooterPosition"; //* Exporto este Componente Funcional */
+export * from "./FooterCard"; //* Exporto este Componente Funcional */
+export * from "./FooterSearch"; //* Exporto este Componente Funcional */
